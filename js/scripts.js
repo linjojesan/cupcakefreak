2f964f11c94567dba97a47f4b3d09aaa4f13eb63
linjojesan <urielpro78@gmail.com>
2017-02-08 17:30:08 -0800

    added button and created js query function to change background when clicked

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+$("button").click(function() {
+$("body").css("background-image","url(http://3.bp.blogspot.com/-fgN0uxbL29U/URqUeo3wyyI/AAAAAAAABI0/_ZJiKG-Xiyc/s1600/luvluvluv.png)");
+})
+});
